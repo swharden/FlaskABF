392afa08551b7a24dbb815780de9ed6ce347a509
Scott W Harden <swharden@gmail.com>
2019-07-30 12:05:53 -0400

    Update origin.py

--- src/abfBrowse/pages/origin.py
+++ src/abfBrowse/pages/origin.py
@@ -7,7 +7,7 @@ def generateHtml(pathLocal):
     abfFolder = abfBrowse.AbfFolder(pathLocal)
     html=""
     if len(abfFolder.abfList.abfIDs):
-        html += "origin commands for an ABF folder"
+        html += "This section is under development. Eventually it will let you create custom copy/paste blocks of Origin commands."
     else:
         html += "this is not an ABF folder..."
     return abfBrowse.htmlTools.htmlPageWrap(html)
